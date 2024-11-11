4988dadf848a3cce68d466574ea7cab01a952173
sromano87 <siromano@unisa.it>
2024-11-11 12:12:43 +0100

    Small changes

--- src/intelligentoffice.py
+++ src/intelligentoffice.py
@@ -1,16 +1,13 @@
-
 import time
-
-from mock.adafruit_veml7700 import VEML7700
 
 DEPLOYMENT = False  # This variable is to understand whether you are deploying on the actual hardware
 
 try:
     import RPi.GPIO as GPIO
-    DEPLOYMENT = True
     import SDL_DS3231
     import board
     import adafruit_veml7700
+    DEPLOYMENT = True
 except:
     import mock.GPIO as GPIO
     import mock.SDL_DS3231 as SDL_DS3231
@@ -20,14 +17,14 @@ except:
 
 class IntelligentOffice:
 
-    INFRARED_PIN1 = 11
-    INFRARED_PIN2 = 12
-    INFRARED_PIN3 = 13
-    INFRARED_PIN4 = 15
-    SERVO_PIN = 18
-    LED_PIN = 29
-    GAS_PIN = 31
-    BUZZER_PIN = 36
+    INFRARED_PIN1 = 11 # First infrared distance sensor pin
+    INFRARED_PIN2 = 12 # Second infrared distance sensor pin
+    INFRARED_PIN3 = 13 # Third infrared distance sensor pin
+    INFRARED_PIN4 = 15 # Fourth infrared distance sensor pin
+    SERVO_PIN = 18 # Servo motor pin
+    LED_PIN = 29 # Light pin
+    GAS_PIN = 31 # Gas/smoke sensor pin
+    BUZZER_PIN = 36 # Active buzzer pin
 
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
@@ -41,7 +38,8 @@ class IntelligentOffice:
         GPIO.setup(self.GAS_PIN, GPIO.IN)
         GPIO.setup(self.BUZZER_PIN, GPIO.OUT)
 
-        self.rtc = SDL_DS3231.SDL_DS3231(1, 0x68)
+        self.rtc = SDL_DS3231.SDL_DS3231(1, 0x68) # rtc
+
         self.servo = GPIO.PWM(self.SERVO_PIN, 50)
         self.servo.start(2)  # Starts generating PWM on the pin with a duty cycle equal to 2% (corresponding to 0 degree)
         if DEPLOYMENT:  # Sleep only if you are deploying on the actual hardware
@@ -49,7 +47,7 @@ class IntelligentOffice:
         self.servo.ChangeDutyCycle(0)  # Sets duty cycle equal to 0% (corresponding to a low signal)
 
         i2c = board.I2C()
-        self.ambient_light_sensor = VEML7700(i2c, 0x10)
+        self.ambient_light_sensor = adafruit_veml7700.VEML7700(i2c, 0x10) # ambient light sensor
 
         self.blinds_open = False
         self.light_on = False
