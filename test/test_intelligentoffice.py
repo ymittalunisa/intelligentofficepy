764a93b97a2ba798fd55d4168867f7a6d035e007
ymittalunisa <y.mittal@studenti.unisa.it>
2024-11-27 17:32:46 +0100

    Red Phase - US1

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -13,3 +13,12 @@ class TestIntelligentOffice(unittest.TestCase):
     def test_something(self, mock_object: Mock):
         # This is an example of test where I want to mock the GPIO.input() function
         pass
+
+    @patch.object(GPIO, "input")
+    def test_check_quadrant_occupancy(self, mock_infra_pin1: Mock, mock_infra_pin2: Mock, mock_infra_pin3: Mock, mock_infra_pin4: Mock):
+        mock_infra_pin1.return_value = True
+        mock_infra_pin2.return_value = True
+        mock_infra_pin3.return_value = True
+        mock_infra_pin4.return_value = True
+        inteloffice = IntelligentOffice()
+        self.assertFalse(inteloffice.check_quadrant_occupancy(mock_infra_pin1))
